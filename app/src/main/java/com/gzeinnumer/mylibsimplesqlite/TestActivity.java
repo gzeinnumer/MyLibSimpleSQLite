de17975ed87549d34e84572d676771bc2d5a8f36
Zein <m.fadli.zein.17@gmail.com>
2020-11-19 14:27:07 +0700

    README.md

--- app/src/main/java/com/gzeinnumer/mylibsimplesqlite/TestActivity.java
+++ app/src/main/java/com/gzeinnumer/mylibsimplesqlite/TestActivity.java
@@ -24,21 +24,22 @@ public class TestActivity extends AppCompatActivity {
 
         Table1 table1 = new Table1();
 
-        List<Table1> list = table1.read();
-        Log.d(TAG, "onCreate1: "+list.get(0).getName());
-        Log.d(TAG, "onCreate1: "+list.size());
+//        List<Table1> list = table1.read();
+//        Log.d(TAG, "onCreate1: "+list.get(0).getName());
+//        Log.d(TAG, "onCreate1: "+list.size());
 
-        List<Table1> listQuery = table1.query();
-        Log.d(TAG, "onCreate2: "+listQuery.get(0).getName());
-        Log.d(TAG, "onCreate2: "+listQuery.get(0).getTable2_name());
+//        List<Table1> listQuery = table1.query();
+//        Log.d(TAG, "onCreate2: "+listQuery.get(0).getName());
+//        Log.d(TAG, "onCreate2: "+listQuery.get(0).getTable2_name());
 
-        boolean istrue = table1.insert(new Table1(500, "Zein", 1.3, "Decs", 1,"2020-12-12"));
+//        boolean istrue = table1.insert(new Table1(500, "Zein", 1.3, "Decs", 1,"2020-12-12"));
+        boolean istrue = table1.insert(new Table1("Zein", 1.3, "Decs", 1,"2020-12-12"));
         Log.d(TAG, "onCreate3: "+istrue);
 
-        boolean istrue2 = table1.update(new Table1(500, "Zein", 1.6, "Decs", 1,"2020-11-11"));
-        Log.d(TAG, "onCreate4: "+istrue2);
+//        boolean istrue2 = table1.update(new Table1(500, "Zein", 1.6, "Decs", 1,"2020-11-11"));
+//        Log.d(TAG, "onCreate4: "+istrue2);
 
-        int count = table1.count();
-        Log.d(TAG, "onCreate5: "+count);
+//        int count = table1.count();
+//        Log.d(TAG, "onCreate5: "+count);
     }
 }
