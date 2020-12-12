14bfe9423bce623aba5e37f77d76877d0b10120c
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2020-12-12 15:13:13 +0700

    README.md

--- app/src/main/java/com/gzeinnumer/mylibsimplesqlite/TestActivity.java
+++ app/src/main/java/com/gzeinnumer/mylibsimplesqlite/TestActivity.java
@@ -26,33 +26,33 @@ public class TestActivity extends AppCompatActivity {
         Table1 table1 = new Table1();
 
         boolean istrue = table1.insert();
-        Log.d(TAG, "onCreate3: "+istrue);
+        Log.d(TAG, "onCreate1: "+istrue);
 
         boolean istrue2 = table1.update();
-        Log.d(TAG, "onCreate4: "+istrue2);
+        Log.d(TAG, "onCreate2: "+istrue2);
 
         boolean istrue3 = table1.delete();
-        Log.d(TAG, "onCreate6: "+istrue3);
+        Log.d(TAG, "onCreate3: "+istrue3);
 
         int count = table1.count();
-        Log.d(TAG, "onCreate5: "+count);
+        Log.d(TAG, "onCreate4: "+count);
 
         int count2 = table1.count2();
         Log.d(TAG, "onCreate5: "+count2);
 
         List<Table1> read = table1.read();
-        Log.d(TAG, "onCreate1: "+read.get(0).getName());
-        Log.d(TAG, "onCreate1: "+read.size());
+        Log.d(TAG, "onCreate6: "+read.get(0).getName());
+        Log.d(TAG, "onCreate6: "+read.size());
 
         List<Table1> read2 = table1.read2();
-        Log.d(TAG, "onCreate1: "+read2.get(0).getName());
-        Log.d(TAG, "onCreate1: "+read2.size());
+        Log.d(TAG, "onCreate7: "+read2.get(0).getName());
+        Log.d(TAG, "onCreate7: "+read2.size());
 
         List<Table1> listQuery = table1.query();
-        Log.d(TAG, "onCreate2: "+listQuery.get(0).getName());
-        Log.d(TAG, "onCreate2: "+listQuery.get(0).getTable2_name());
+        Log.d(TAG, "onCreate8: "+listQuery.get(0).getName());
+        Log.d(TAG, "onCreate8: "+listQuery.get(0).getTable2_name());
 
         boolean queryUpdate = table1.queryResultUpdate();
-        Log.d(TAG, "onCreate2: "+queryUpdate);
+        Log.d(TAG, "onCreate9: "+queryUpdate);
     }
 }
